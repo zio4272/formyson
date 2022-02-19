f502842d6dce74d0feaa5276b43aca7b64473443
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-02-20 02:01:57 +0900

    auth 관리자 페이지 연결

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    # 상세화면에 보여줄 필드 설정 없는경우 전체 보여줌
+    # fields = ['username']
+    # 목록에 보여줄 데이터
+    list_display = ('username', 'email') 
+    
+
+admin.site.register(User, UserAdmin)
