d69da67ac2a83a4dac6beab80ee90ed3053b58dd
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-02-20 01:51:53 +0900

    authentication app 생성
    회원가입 생성

---
+++ authentication/serializers.py
@@ -0,0 +1,22 @@
+from django.forms import ValidationError
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        username = attrs.get('username', '')
+
+        if not username.isalnum():
+            raise serializers.ValidationError('사용자 이름은 영숫자 문자만 포함해야 합니다.')
+
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
---
+++ authentication/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics, status
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_calss = RegisterSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_calss(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        
+        return Response(user_data, status=status.HTTP_201_CREATED)
