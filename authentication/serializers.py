93b2a832ecaf77763417dac092c0023c7e65f911
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-02-20 13:52:10 +0900

    로그인 구현

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
@@ -19,3 +21,34 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(max_length=255, min_length=3, read_only=True)
+    tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username', 'tokens']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+
+        user = auth.authenticate(email=email, password=password)
+
+        if not user:
+            raise AuthenticationFailed('사용자 정보가 존재하지 않습니다.')
+
+        if not user.is_active:
+            raise AuthenticationFailed('활성화 되지 않은 사용자 입니다.')
+
+        if not user.is_verified:
+            raise AuthenticationFailed('승인되지 않은 사용자 입니다.')
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens()
+        }
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 
 class RegisterView(generics.GenericAPIView):
@@ -14,3 +14,10 @@ class RegisterView(generics.GenericAPIView):
         user_data = serializer.data
         
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
