96793373b67a7c68cc626337400a19c2101e5321
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-02-20 14:57:16 +0900

    post serializer 수정
    리턴값 id 추가

--- post/serializers.py
+++ post/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Post
 
 class PostListSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Post
-        fields = ['user_id', 'title', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at']
