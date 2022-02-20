3cb0b722da1dc41269ce8e636efbc59dd0cbfb3c
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-02-20 14:07:51 +0900

    토큰 분리

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -26,11 +26,12 @@ class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=3)
     password = serializers.CharField(max_length=68, min_length=6, write_only=True)
     username = serializers.CharField(max_length=255, min_length=3, read_only=True)
-    tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+    refresh_token = serializers.CharField(max_length=68, min_length=6, read_only=True)
+    access_token = serializers.CharField(max_length=68, min_length=6, read_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'username', 'tokens']
+        fields = ['email', 'password', 'username', 'refresh_token', 'access_token']
 
     def validate(self, attrs):
         email = attrs.get('email', '')
@@ -50,5 +51,6 @@ class LoginSerializer(serializers.ModelSerializer):
         return {
             'email': user.email,
             'username': user.username,
-            'tokens': user.tokens()
+            'refresh_token': user.tokens()['refresh'],
+            'access_token': user.tokens()['access']
         }
