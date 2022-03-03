120a77459446f98d5ee71facc9345782f7e7350b
zio4272 <30651502+zio4272@users.noreply.github.com>
2022-03-03 23:46:28 +0900

    Update serializers.py
    post 등록시 images array 첨부가능 하도록 변경
    
    1. 이미지 등록
    2. return url image 데이터로 활용

--- post/serializers.py
+++ post/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from .models import Post, Comment, PostImages
+from rest_framework.response import Response
 
 class PostImagesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -41,18 +42,31 @@ class PostSerializer(serializers.ModelSerializer):
             
     comments = CommentSerializer(read_only=True, many=True)
     comment_count = serializers.IntegerField(source='comments.count', read_only=True)
-    images = PostImagesSerializer(read_only=True, many=True)
+    images = PostImagesSerializer(many=True, required=False)
     
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'user', 'comments', 'comment_count', 'images']
 
 class PostBodySerializer(serializers.ModelSerializer):
-    images = serializers.ImageField()
+    images = serializers.ListField(required=False)
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'images']
+        fields = ['title', 'content', 'created_at', 'updated_at', 'images']
         examples = {
             'title': '제목',
-            'content': '내용'
+            'content': '내용',
+            'images': [
+                'a',
+                'b'
+            ]
         }
+
+    def create(self, validated_data):
+        if 'images' in validated_data:
+            images_data = validated_data.pop('images')
+            post = Post.objects.create(**validated_data)
+            for image in images_data:
+                PostImages.objects.create(post_id=post, image=image)
+        return self.data
